f4e281567c7279268be6a4ce9b35c6438ecaf12a
Akide Liu <mail@llycloud.com>
2020-09-10 12:22:16 +0930

    Add Pre exam 03 pr12

---
+++ Practical/practical_exam/exam3-PR12/Unit.h
@@ -0,0 +1,20 @@
+// Author : Akide Liu 
+// Date : 10/9/20
+// License and copyright notice: GNU General Public License v3.0 
+// https://www.gnu.org/licenses/gpl-3.0.en.html
+// Description : 
+
+//
+
+#ifndef CPP_UNIT_H
+#define CPP_UNIT_H
+
+
+
+class Unit {
+
+};
+
+
+
+#endif //CPP_UNIT_H
